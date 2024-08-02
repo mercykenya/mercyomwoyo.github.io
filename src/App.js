925f5f8a04eca8d5b37a3b4a200e73e917e25494
Mercy Omwoyo <mercyomwoyo@dhcp-10-105-12-182.wireless.northwestern.private>
2024-08-02 16:30:33 -0500

    Prepare for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import './App.css';
 
 import CodeVisualizer from './CodeVisualizer';
 import CSExplorationHub from './CSExplorationHub';
-import ComputerScienceQuiz from './ComputerScienceQuiz'; // Import the ComputerScienceQuiz component
+import ComputerScienceQuiz from './ComputerScienceQuiz';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
             <ul>
               <li><Link to="/code-playground">Code Playground</Link></li>
               <li><Link to="/cs-exploration">CS Exploration Hub</Link></li>
-              <li><Link to="/quiz">Quiz</Link></li> {/* Link to ComputerScienceQuiz */}
+              <li><Link to="/quiz">Quiz</Link></li>
             </ul>
           </nav>
         </header>
@@ -24,22 +24,8 @@ function App() {
           <Routes>
             <Route path="/code-playground" element={<CodeVisualizer />} />
             <Route path="/cs-exploration" element={<CSExplorationHub />} />
-            <Route path="/quiz" element={<ComputerScienceQuiz />} /> {/* Route for ComputerScienceQuiz */}
-            <Route path="/" element={
-              <div className="intro">
-                <h2>Welcome to Mercy Omwoyo's CS Playground!</h2>
-                <p>
-                  Get into the exciting world of computer science with our interactive tools designed to make learning and exploration fun. 
-                  Here, you can:
-                </p>
-                <ul>
-                  <li><strong>Experiment with Code:</strong> Use our <Link to="/code-playground">Code Playground</Link> to write, run, and visualize code in various programming languages. See your code in action and learn through interactive experimentation.</li>
-                  <li><strong>Explore Computer Science Concepts:</strong> Visit our <Link to="/cs-exploration">CS Exploration Hub</Link> to engage with interactive quizzes, educational games, and other fun activities that help you understand and appreciate computer science.</li>
-                  <li><strong>Test Your Knowledge:</strong> Try out our <Link to="/quiz">Quiz</Link> to see how much you know and learn new facts about computer science.</li>
-                </ul>
-                <p>Whether you are a beginner or a seasoned coder, there’s something here for everyone. Enjoy exploring and learning!</p>
-              </div>
-            } />
+            <Route path="/quiz" element={<ComputerScienceQuiz />} />
+            <Route path="/" element={<Navigate to="/code-playground" />} />
           </Routes>
         </main>
       </div>
