0b7fa3810647aac14d5e8b6db4ef61610be75d04
Mercy Omwoyo <mercyomwoyo@dhcp-10-105-12-182.wireless.northwestern.private>
2024-08-02 16:56:14 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import './App.css';
-
+import Welcome from './Welcome';
 import CodeVisualizer from './CodeVisualizer';
 import CSExplorationHub from './CSExplorationHub';
 import ComputerScienceQuiz from './ComputerScienceQuiz';
 
+
 function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    // Automatically navigate to CSExplorationHub after 10 seconds
+    const timer = setTimeout(() => {
+      setShowWelcome(false);
+    }, 10000); // 10 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Router>
       <div className="App">
@@ -25,7 +37,7 @@ function App() {
             <Route path="/code-playground" element={<CodeVisualizer />} />
             <Route path="/cs-exploration" element={<CSExplorationHub />} />
             <Route path="/quiz" element={<ComputerScienceQuiz />} />
-            <Route path="/" element={<Navigate to="/code-playground" />} />
+            <Route path="/" element={showWelcome ? <Welcome /> : <Navigate to="/cs-exploration" />} />
           </Routes>
         </main>
       </div>
