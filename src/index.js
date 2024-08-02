db9066c6f3eb49ad76643ec3375d3e1751ef2448
Mercy Omwoyo <mercyomwoyo@dhcp-10-105-12-182.wireless.northwestern.private>
2024-08-02 16:04:14 -0500

    Finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';  // Make sure this line is correct
+import App from './App';  // Assuming App.js is directly under src/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
